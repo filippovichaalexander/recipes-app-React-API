77c362657171314587f4bb151db62e7b51fa4ab5
unknown <alex-fil@mail.ru>
2020-12-27 23:26:07 +0300

    Updates

---
+++ precache-manifest.8920d1de28369c621a84cd3a5771e904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fc866edd46bd10849dafaf0d933e42",
+    "url": "/recipes-app-React-API/index.html"
+  },
+  {
+    "revision": "c1b9a207f4d3b9b1243e",
+    "url": "/recipes-app-React-API/static/css/main.093836e1.chunk.css"
+  },
+  {
+    "revision": "d35d056d85b507874b4e",
+    "url": "/recipes-app-React-API/static/js/2.05c48724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-app-React-API/static/js/2.05c48724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b9a207f4d3b9b1243e",
+    "url": "/recipes-app-React-API/static/js/main.e8d3c80d.chunk.js"
+  },
+  {
+    "revision": "07bfc6aeaff439dad46b",
+    "url": "/recipes-app-React-API/static/js/runtime-main.f5b986e8.js"
+  }
+]);
